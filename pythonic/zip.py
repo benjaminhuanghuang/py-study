9ab81e03d36410fb02e8e475fbc6f20c168b6f60
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-12-04 18:21:23 -0800

    zip function

--- pythonic/zip.py
+++ pythonic/zip.py
@@ -6,3 +6,14 @@ print zip(x_list, y_list)
 
 print dict(zip(x_list, y_list))
 
+names = ['raymond', 'rachel', 'mathew']
+colors = ['red', 'green', 'blue','yellow']
+
+# bad
+n = min (len(names), len(colors))
+for i in range(n):
+    print names[i], "->", colors[i]
+
+# better
+for name, color in zip(names, colors):
+    print name, "->", color
