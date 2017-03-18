556d75f7f1aefc4c2f3712a5fed654f9e0f06e73
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-18 11:20:17 -0700

    add comments

--- decorator/decorator_step3.py
+++ decorator/decorator_step3.py
@@ -3,7 +3,7 @@
 
 http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000
 https://www.kancloud.cn/wizardforcel/liaoxuefeng/108528
-
+http://www.cnblogs.com/rhcad/archive/2011/12/21/2295507.html
 
 purpose:  Enhance a function but do not change the definition of the function
 '''
