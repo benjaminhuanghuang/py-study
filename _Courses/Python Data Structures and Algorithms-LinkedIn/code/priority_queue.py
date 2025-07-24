9a4735bc0849daaec2afc1967d7cdb5c669e75e1
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-07-24 09:08:00 -0700

    update

--- _Courses/Python Data Structures and Algorithms-LinkedIn/code/priority_queue.py
+++ _Courses/Python Data Structures and Algorithms-LinkedIn/code/priority_queue.py
@@ -2,10 +2,44 @@
 Python Data Structures - A Game-Based Approach
 Priority Queue Class based on heapq.
 Robin Andrews - https://compucademy.net/
+
+Python's heapq module only implements min heaps, where the smallest element is always at the root (index 0).
 """
 
 import heapq
 
 
 class PriorityQueue:
-    pass
+    def __init__(self):
+        self.elements = []
+
+    def is_empty(self):
+        return not self.elements
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+    def __str__(self):
+        return str(self.elements)
+
+
+if __name__ == "__main__":
+    pq = PriorityQueue()
+    print(pq)
+    print(pq.is_empty())
+
+    # item, priority
+    pq.put("eat", 2)
+    pq.put("code", 1)
+    pq.put("sleep", 3)
+
+    print(pq)
+
+    print(pq.get())
+    print(pq.get())
+    print(pq.get())
+
+    print(pq)
